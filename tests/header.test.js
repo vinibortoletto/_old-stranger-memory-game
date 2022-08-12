20b6382497bb0cc6ca054ad1eb698ca76e3eca05
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-08-12 16:13:21 -0300

    initial configuration

---
+++ tests/header.test.js
@@ -0,0 +1,7 @@
+import { updateTapes, tapesElement } from '../scripts/header';
+// const { updateTapes, tapesElement } = require('../scripts/header');
+
+describe('tapes', () => {
+  it('1. should start as 0', () => {
+  });
+});
