e515b1a880cab244b678b8980d9264294ab663c4
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-08-15 19:16:51 -0300

    config: folder structure

--- script.js
+++
@@ -1,5 +0,0 @@
-// import startTimer from './scripts/startTimer.js';
-
-window.onload = () => {
-  // startTimer();
-};
--- scripts/updateScore.js
+++
@@ -1,8 +0,0 @@
-const updateScore = (id) => {
-  const element = document.getElementById(id);
-  let counter = Number(element.innerHTML);
-  counter += 1;
-  element.innerText = counter;
-};
-
-export default updateScore;
